d3ef807b2b72c9c09ba6bdb70b28e63e1f0e9ad1
alindaByamukama <alindabyamukama@gmail.com>
2023-02-20 19:19:09 +0300

    tdd-cycle-4 and tdd-cycle-5

--- test/testMultiply.js
+++ test/testMultiply.js
@@ -13,4 +13,12 @@ describe('Test our multiply function', () => {
   it('3 multiplied by 3 == 9', () => {
     assert.equal(multiply(3, 3), 9);
   });
+    
+  it('4 multiplied by 4 == 16', () => {
+    assert.equal(multiply(4, 4), 16);
+  });
+    
+  it('23 multiplied 45 == 23 * 45 (1035)', () => {
+    assert.equal(multiply(23, 45), 23 * 45);
+  });
 });
