703525e61fa2630fb6a355702065beb4e817865e
alindaByamukama <alindabyamukama@gmail.com>
2023-02-21 22:47:23 +0300

    create fibonacci function finds index from value

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,6 @@
 // add two numbers in a loop - reassign each item with the next value
 
-const fibonacci = (n) => {
+const fibonacciIndexLoop = (n) => {
   let a = 0;
   let b = 1;
   let c = n;
@@ -13,4 +13,27 @@ const fibonacci = (n) => {
   return c;
 };
 
-exports.fibonacci = fibonacci;
+// take a number and return its index - if it's 0 or 1 we return that same value
+
+const fibonacciNumLoop = (n) => {
+  if (n <= 1) {
+    return n;
+  }
+
+  let a = 0;
+  let b = 1;
+  let c = n;
+  let result = 1;
+
+  while (c < n) {
+    c = a + b;
+    a = b;
+    b = c;
+    result += 1;
+  }
+
+  return result;
+};
+
+exports.fibonacciIndexLoop = fibonacciIndexLoop;
+exports.fibonacciNumLoop = fibonacciNumLoop;
