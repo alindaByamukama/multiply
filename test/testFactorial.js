7191fbc478b0fe02045c11c292b056818bdfb426
alindaByamukama <alindabyamukama@gmail.com>
2023-02-21 19:38:13 +0300

    adds tests for negative nummber and decimals

--- test/testFactorial.js
+++ test/testFactorial.js
@@ -2,8 +2,20 @@ const { assert } = require('chai');
 const { factorialize } = require('../factorial');
 
 describe('Test our factorial function', () => {
+  it('fails if integer is a decimal', () => {
+    assert.equal(factorialize(0.75), 'integer must be a whole number');
+  });
+
+  it('fails if integer is a negative number', () => {
+    assert.equal(factorialize(-1), 'integer must be a positve number');
+  });
+
   it('factorial 0 returns 1', () => {
     assert.equal(factorialize(0), 1);
+  });
+
+  it('factorial 1 returns 1', () => {
+    assert.equal(factorialize(1), 1);
   });
 
   it('factorial 5 returns 120', () => {
