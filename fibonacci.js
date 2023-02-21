375292dc19c523d32c05852e8ca249225dd3961b
alindaByamukama <alindabyamukama@gmail.com>
2023-02-21 21:59:42 +0300

    create for loop function to meet test requirements

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = () => {
-  // calculate somthing here
+// add two numbers in a loop - reassign each item with the next value
+
+const fibonacci = (n) => {
+  let a = 0;
+  let b = 1;
+  let c = n;
+
+  for (let index = 2; index <= n; index += 1) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 };
 
 exports.fibonacci = fibonacci;
